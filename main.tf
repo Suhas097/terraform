707b0ece7560fc639870ef512df9e31675c85fe3
Suhas097 <suhaslyagalwar2838@gmail.com>
2025-05-26 15:45:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ region = "us-east-1"
 }
 resource "aws_instance" "suhas" {
 count = 3
-  ami           = "ami-071226ecf16aa7d96"
-  instance_type = "t2.micro"
+  ami           = ""
+  instance_type = "t2.micro"ami-0953476d60561c955
   tags = {
     Name = "HCP-server"
   }
